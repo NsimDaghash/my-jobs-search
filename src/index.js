49bd42a64ed31969830465369acfa1472705e529
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2024-04-26 15:01:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/repo-name">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
